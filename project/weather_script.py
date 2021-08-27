3ac0e9f85413379bc611a685a919b6b5f6888609
Astony <savrushkin.aa@gmail.com>
2021-08-27 19:39:46 +0300

    add weather api funcs, create plot and csv files

--- project/weather_script.py
+++ project/weather_script.py
@@ -7,12 +7,16 @@ from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 from geopy.extra.rate_limiter import RateLimiter
 import math
-
-
-
-
+import requests
+import json
+from datetime import date, datetime
+import time
+from celery import shared_task
+import matplotlib.pyplot as plt
 
 API_KEY = 'dYrajXLId5Rnp_WKZHaonsR-SMy2Z0xOlPRM4uELmfY'
+
+key = 'af194677845bda5d5065ebc3a0093a12'
 
 
 def filter(df):
@@ -90,9 +94,84 @@ def get_coordinates_of_central(dataframe):
     return math.degrees(central_latitude), math.degrees(central_longitude)
 
 
-@logger.catch
+
+
 def calculate_central_area():
     """Function that returns dictionary with city and it's center"""
-    df = define_address()
+    df = pd.read_csv("results.csv")
     return {city:get_coordinates_of_central(dataframe) for city, dataframe in df.groupby(["City"])}
+
+
+def fetch(url):
+    return requests.get(url)
+
+
+
+
+@logger.catch
+def forecast_weather():
+    cities_and_coordinate = calculate_central_area()
+    forecast_weather_list =[]
+    cities = list(np.repeat([city for city in cities_and_coordinate],8))
+    coordinates = [coordinate for coordinate in cities_and_coordinate.values()]
+    urls = [f"https://api.openweathermap.org/data/2.5/onecall?lat={coordinate[0]}&lon={coordinate[1]}&exclude={'hourly'}&appid={key}" for coordinate in coordinates]
+    with ThreadPoolExecutor(max_workers=len(urls)) as pool:
+        responses = pool.map(fetch, urls)
+    for response in responses:
+        weather_per_week = eval(response.text)['daily']
+        for day in weather_per_week:
+            forecast_weather_list.append({
+                "City":cities.pop(0),
+                "min_temp":day["temp"]["min"],
+                "max_temp": day["temp"]["max"],
+                "day":datetime.utcfromtimestamp(day["dt"]).strftime('%Y-%m-%d')})
+    with open("forecast_weather_results2.json", "w") as file:
+        json.dump(forecast_weather_list, file)
+
+
+def prev_5_days():
+    current_date = int(datetime.timestamp(datetime.today()))
+    return [current_date - index * 86400 for index in range(1,6)]
+@logger.catch
+def prev_weather():
+    urls = []
+    day_list = prev_5_days()
+    cities_and_coordinates = calculate_central_area()
+    cities = list(np.repeat([city for city in cities_and_coordinates],5))
+    coordinates = [coordinate for coordinate in cities_and_coordinates.values()]
+    weather_list = []
+    for coordinate in coordinates:
+        for timestamp in day_list:
+            urls.append(f"https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={coordinate[0]}&lon={coordinate[1]}&dt={timestamp}&appid={key}")
+    with ThreadPoolExecutor(max_workers=len(urls)) as pool:
+        responses = pool.map(fetch, urls)
+    for response in responses:
+        weather_per_day = eval(response.text)
+        daily_temperature = [element['temp'] for element in weather_per_day['hourly']]
+        weather_list.append({"City":cities.pop(0),
+                            "min_temp":min(daily_temperature),
+                            "max_temp":max(daily_temperature),
+                            "day":datetime.utcfromtimestamp(weather_per_day['hourly'][0]['dt']).strftime('%Y-%m-%d')})
+
+
+
+def create_plots(df):
+    for city, dataframe in df.groupby(df["City"]):
+        df = dataframe.pivot_table(["max_temp", "min_temp"],"day")
+        print(city, df)
+        df.plot(kind = 'bar')
+        plt.ylabel("Temperature in Celsius")
+        plt.title(f"{city}")
+        plt.show()
+df = pd.read_csv("results.csv")
+
+
+def create_csv(df):
+    for city, dataframe in df.groupby(df["City"]):
+        dataframe.to_csv(f"{city}.csv")
+
+
+
+
+
 
